d3e3d600533c489f9d29708ef880f4328d8ff855
hiranmaiti <hiran.maiti@gmail.com>
2024-08-05 14:29:02 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,29 @@
+gsap.from("#img", {
+  x: 100,
+  opacity: 0,
+  rotate: 360,
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const progressBars = document.querySelectorAll(".progress-bar");
+  
+    const options = {
+      threshold: 0.5
+    };
+  
+    const callback = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const progressBar = entry.target;
+          const width = progressBar.getAttribute("data-width");
+          progressBar.style.width = width;
+        }
+      });
+    };
+  
+    const observer = new IntersectionObserver(callback, options);
+  
+    progressBars.forEach((progressBar) => {
+      observer.observe(progressBar);
+    });
+  });
